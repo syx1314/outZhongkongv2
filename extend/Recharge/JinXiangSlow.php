7f8c0917a91f674fd672e2a205e593c68871a64c
3154971393@qq.com <694073asd>
2021-10-08 19:47:16 +0800

    金翔

---
+++ extend/Recharge/JinXiangSlow.php
@@ -0,0 +1,105 @@
+<?php
+
+
+namespace Recharge;
+
+use think\Log;
+
+/**
+ * Class JinXiangSlow
+ * @package Recharge  金翔慢充  wx:trosft66
+ */
+class JinXiangSlow
+{
+    private $szAgentId;//商户编号
+    private $szKey;
+    private $notify;
+    private $apiurl;//话费充值接口
+
+    public function __construct($option)
+    {
+        $this->szAgentId = isset($option['param1']) ? $option['param1'] : '';
+        $this->szKey = isset($option['param2']) ? $option['param2'] : '';
+        $this->notify = isset($option['param3']) ? $option['param3'] : '';
+        $this->apiurl = isset($option['param4']) ? $option['param4'] : '';
+    }
+
+    /**
+     * 提交充值号码充值
+     */
+    public function recharge($out_trade_num, $mobile, $param, $isp = '')
+    {
+        $data = [
+            "merchantId" => $this->szAgentId,
+            "orderType" => 2, //订单类型 1-快充 2-慢充
+            "tradeNoThird" => $out_trade_num,
+            "mobile" => $mobile,
+            "amount" => $param['param1'].'.00',
+            'timeout' => time()*1000+15552000000 // 72小时超时时间
+        ];
+        $data['notifyUrl'] = $this->notify;
+        $signstr =$data['merchantId'].$data['orderType'].$data['mobile'].$data['tradeNoThird'].$data['amount'].$data['notifyUrl'].$data['timeout'].$this->szKey;
+        Log::error($signstr);
+        $sign = md5($signstr);
+        Log::error($sign);
+        $data['signstr'] = $sign;
+        return $this->http_post($this->apiurl, $data);
+    }
+
+    private function get_teltype($str)
+    {
+        switch ($str) {
+            case '移动':
+                return 1;
+            case '联通':
+                return 2;
+            case '电信':
+                return 3;
+            default:
+                return -1;
+        }
+    }
+
+    /**
+     * get请求
+     * @param $methond
+     * @param $param
+     * @return bool|mixed
+     */
+    private function http_post($url, $param)
+    {
+        $oCurl = curl_init();
+        if (stripos($url, "https://") !== FALSE) {
+            curl_setopt($oCurl, CURLOPT_SSL_VERIFYPEER, FALSE);
+            curl_setopt($oCurl, CURLOPT_SSL_VERIFYHOST, false);
+            curl_setopt($oCurl, CURLOPT_SSLVERSION, 1);
+        }
+        if (is_string($param)) {
+            $strPOST = $param;
+        } else {
+            $strPOST = urldecode(http_build_query($param));
+        }
+        curl_setopt($oCurl, CURLOPT_URL, $url);
+        curl_setopt($oCurl, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($oCurl, CURLOPT_POST, true);
+        curl_setopt($oCurl, CURLOPT_POSTFIELDS, $strPOST);
+        curl_setopt($oCurl, CURLOPT_CONNECTTIMEOUT, 10);
+        curl_setopt($oCurl, CURLOPT_TIMEOUT, 60);
+        curl_setopt($oCurl, CURLOPT_HEADER, 0);
+        // curl_setopt($oCurl, CURLOPT_HTTPHEADER, array('Content-Type:application/x-www-form-urlencoded;charset=utf-8'));
+        $sContent = curl_exec($oCurl);
+        $aStatus = curl_getinfo($oCurl);
+        Log::error("网厅返回：".$sContent);
+        curl_close($oCurl);
+        if (intval($aStatus["http_code"]) == 200) {
+            $result = json_decode($sContent, true);
+            if ($result['code'] == 200) {
+                return rjson(0, $result['code'].$result['msg'], $result);
+            } else {
+                return rjson(1, $result['code'].$result['msg'], $result);
+            }
+        } else {
+            return rjson(1, '接口访问失败，http错误码' . urldecode(http_build_query($aStatus)));
+        }
+    }
+}
